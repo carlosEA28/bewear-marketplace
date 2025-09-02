0c30404565c2f0b3295171f8a1f54faeab348724
carlosEA28 <carloseaduarte28@gmail.com>
2025-09-02 10:12:15 -0300

    feat: add product list and product item component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
-import Header from "@/components/common/header";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-export default function Home() {
+import Header from "@/components/common/header";
+import ProductListComponent from "@/components/common/product-list";
+import { db } from "@/db";
+
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: { variants: true },
+  });
+
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-4">
+          <Image
+            src={"/banner.png"}
+            alt="leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductListComponent products={products} title="Mais Vendidos" />
+        <div className="px5">
+          <Image
+            src={"/banner-2.png"}
+            alt="leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/common/product-item.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { productTable, productVariantTable } from "@/db/schema";
+import { formatCents } from "@/helpers/money";
+
+interface ProductItemProps {
+  product: typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  };
+}
+
+const ProductItemComponent = ({ product }: ProductItemProps) => {
+  const firstVariant = product.variants[0];
+  return (
+    <Link href={"#"} className="flex flex-col gap-4">
+      <Image
+        src={firstVariant.imageUrl}
+        alt={firstVariant.name}
+        width={200}
+        height={200}
+        className="rounded-3xl"
+      />
+
+      <div className="flex flex-col max-w-[200px] gap-1">
+        <p className="truncate text-sm font-medium">{product.name}</p>
+        <p className="truncate text-xs text-muted-foreground font-medium">
+          {product.description}
+        </p>
+        <p className="truncate text-sm font-semibold">
+          {formatCents(firstVariant.priceInCents)}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductItemComponent;
