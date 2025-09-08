343442bf21004ffb64e8e103d9a5e4374bf7cad7
carlosEA28 <carloseaduarte28@gmail.com>
2025-09-08 09:39:37 -0300

    feat: add cart summary component

---
+++ src/app/cart/components/cart-summary.tsx
@@ -0,0 +1,95 @@
+import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { formatCents } from "@/helpers/money";
+
+interface CartSummaryProps {
+  subTotalInCents: number;
+  totalInCents: number;
+  product: Array<{
+    id: string;
+    productName: string;
+    variantName: string;
+    quantity: number;
+    priceInCents: number;
+    imageUrl: string;
+  }>;
+}
+
+const CartSummaryComponent = ({
+  subTotalInCents,
+  totalInCents,
+  product,
+}: CartSummaryProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Seu pedido</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-3">
+        <div className="flex justify-between">
+          <p className="text-sm">Subtotal</p>
+          <p className="text-sm font-semibold">
+            {formatCents(subTotalInCents)}
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-sm">Transporte e Manuseio</p>
+          <p className="text-sm font-semibold">GRÁTIS</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-sm">Taxa Estimada</p>
+          <p className="text-sm font-semibold">-</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-sm">Total</p>
+          <p className="text-sm font-semibold">{formatCents(totalInCents)}</p>
+        </div>
+
+        <div className="py-3">
+          <Separator />
+        </div>
+        <div className="flex flex-col gap-4">
+          {product.map((productItem) => (
+            <div
+              className="flex items-center justify-between"
+              key={productItem.id}
+            >
+              <div className="flex items-center gap-4">
+                <Image
+                  src={productItem.imageUrl}
+                  alt={productItem.variantName}
+                  width={78}
+                  height={78}
+                  className="rounded-lg"
+                />
+
+                <div className="flex flex-col gap-1">
+                  <p className="text-xs font-semibold">
+                    {productItem.productName}
+                  </p>
+                  <p className="text-xs font-medium text-muted-foreground">
+                    {productItem.variantName}
+                  </p>
+                  <p className="text-xs text-muted-foreground">
+                    {productItem.quantity}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col justify-center items-end gap-2 ">
+                <p className="text-sm font-bold">
+                  {formatCents(productItem.priceInCents)}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CartSummaryComponent;
--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -2,11 +2,13 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import { db } from "@/db";
 import { cartTable, shippingAddressTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
+import CartSummaryComponent from "../components/cart-summary";
 import AddressesComponent from "./components/addresses";
 
 const IdentificationPage = async () => {
@@ -21,7 +23,15 @@ const IdentificationPage = async () => {
   const cart = await db.query.cartTable.findFirst({
     where: eq(cartTable.userId, session?.user.id),
     with: {
-      items: true,
+      items: {
+        with: {
+          productVariant: {
+            with: {
+              product: true,
+            },
+          },
+        },
+      },
       shippingAddress: true,
     },
   });
@@ -34,13 +44,32 @@ const IdentificationPage = async () => {
     where: eq(shippingAddressTable.userId, session.user.id),
   });
 
+  const cartTotalPriceInCents = cart.items.reduce(
+    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+    0
+  );
+
   return (
-    <>
+    <div className="space-y-12">
       <Header />
-      <div className="px-5">
+      <div className="px-5 space-y-4">
         <AddressesComponent shippingAddresses={shippingAddresses} />
+
+        <CartSummaryComponent
+          subTotalInCents={cartTotalPriceInCents}
+          totalInCents={cartTotalPriceInCents}
+          product={cart.items.map((item) => ({
+            id: item.productVariant.id,
+            productName: item.productVariant.product.name,
+            variantName: item.productVariant.name,
+            quantity: item.quantity,
+            priceInCents: item.productVariant.priceInCents,
+            imageUrl: item.productVariant.imageUrl,
+          }))}
+        />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
