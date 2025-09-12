a9ed4fead204ff947aa648f5209b4fa1fce3d7ce
carlosEA28 <carloseaduarte28@gmail.com>
2025-09-12 09:08:38 -0300

    feat: ensure user cannot add product to cart if not logged in

--- src/app/product-variant/[slug]/components/add-to-cart-button.tsx
+++ src/app/product-variant/[slug]/components/add-to-cart-button.tsx
@@ -2,9 +2,21 @@
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
+import { useState } from "react";
 
 import { addProductToCart } from "@/actions/add-product-to-cart";
+import SignInFormComponet from "@/app/authentication/components/sign-in-form";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { authClient } from "@/lib/authClient";
+
+import LoginDialogComponent from "./login-dialog";
 
 interface AddToCartButtonProps {
   productVariantId: string;
@@ -15,6 +27,9 @@ const AddToCartButtonComponent = ({
   productVariantId,
   quantity,
 }: AddToCartButtonProps) => {
+  const session = authClient.useSession();
+  const [open, setOpen] = useState(false);
+
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationKey: ["addProductToCart", productVariantId, quantity],
@@ -27,20 +42,47 @@ const AddToCartButtonComponent = ({
       queryClient.invalidateQueries({ queryKey: ["cart"] });
     },
   });
+
+  const handleClick = () => {
+    if (!session.data?.user) {
+      setOpen(true);
+      return;
+    }
+
+    mutate();
+  };
+
   return (
-    <Button
-      className="rounded-full"
-      size={"lg"}
-      variant={"outline"}
-      disabled={isPending}
-      onClick={() => mutate()}
-    >
-      {isPending ? (
-        <Loader2 className="animate-spin mr-1" />
-      ) : (
-        "Adicionar à sacola"
-      )}
-    </Button>
+    <>
+      <Button
+        className="rounded-full"
+        size="lg"
+        variant="outline"
+        disabled={isPending}
+        onClick={handleClick}
+      >
+        {isPending ? (
+          <Loader2 className="animate-spin mr-1" />
+        ) : (
+          "Adicionar à sacola"
+        )}
+      </Button>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Fazer login</DialogTitle>
+            <DialogDescription>
+              Conecte-se à BEWEAR e aproveite uma experiência feita pra quem se
+              veste com personalidade.
+            </DialogDescription>
+          </DialogHeader>
+
+          {/* Seu componente de login real */}
+          <SignInFormComponet />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
